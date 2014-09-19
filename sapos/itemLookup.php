1148380faca99c2bc43de6d31e025f01bb23c3c2
Andrew Rivett <veggiefrog@gmail.com>
2014-09-19 11:54:12 -0400

    Added ability to update pricepoints for open invoices

--- sapos/itemLookup.php
+++ sapos/itemLookup.php
@@ -137,6 +137,73 @@ function ciniki_products_sapos_itemLookup($ciniki, $business_id, $args) {
 		return array('stat'=>'ok', 'item'=>$product);
 	}
 
+	//
+	// Lookup an existing object and pricepoint
+	//
+	if( $args['object'] == 'ciniki.products.product' 
+		&& isset($args['object_id']) && $args['object_id'] > 0
+		&& isset($args['pricepoint_id']) && $args['pricepoint_id'] > 0 
+		) {
+		$strsql = "SELECT ciniki_products.id, "
+			. "ciniki_products.parent_id, "
+			. "ciniki_products.code, "
+			. "ciniki_products.name, "
+			. "ciniki_product_prices.id AS price_id, "
+			. "ciniki_product_prices.pricepoint_id AS pricepoint_id, "
+			. "ciniki_product_prices.name AS price_name, "
+			. "ciniki_product_prices.unit_amount, "
+			. "ciniki_product_prices.unit_discount_amount, "
+			. "ciniki_product_prices.unit_discount_percentage, "
+			. "inventory_flags, inventory_current_num, "
+			. "ciniki_product_prices.taxtype_id "
+			. "FROM ciniki_products "
+			. "LEFT JOIN ciniki_product_prices ON ("
+				. "ciniki_products.id = ciniki_product_prices.product_id "
+				. "AND ciniki_product_prices.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+				. "AND ciniki_product_prices.pricepoint_id = '" . ciniki_core_dbQuote($ciniki, $args['pricepoint_id']) . "' "
+				. ") "
+			. "WHERE ciniki_products.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+			. "AND ciniki_products.id = '" . ciniki_core_dbQuote($ciniki, $args['object_id']) . "' "
+			. "";
+		ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryIDTree');
+		$rc = ciniki_core_dbHashQueryIDTree($ciniki, $strsql, 'ciniki.products', array(
+			array('container'=>'products', 'fname'=>'id',
+				'fields'=>array('id', 'price_id', 'parent_id', 'code', 'description'=>'name',
+					'pricepoint_id', 
+					'unit_amount', 'unit_discount_amount', 'unit_discount_percentage',
+					'inventory_flags', 'inventory_current_num', 
+					'taxtype_id')),
+			));
+		if( $rc['stat'] != 'ok' ) {
+			return $rc;
+		}
+		if( !isset($rc['products']) || count($rc['products']) < 1 ) {
+			return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'2039', 'msg'=>'No product found.'));		
+		}
+		$product = array_pop($rc['products']);
+
+		// Check if product has inventory or unlimited
+		if( ($product['inventory_flags']&0x01) > 0 ) {
+			if( ($product['inventory_flags']&0x02) > 0 ) {
+				$product['limited_units'] = 'no';
+				$product['flags'] = 0x46;	// Inventoried and backorder available
+			} else {
+				$product['limited_units'] = 'yes';
+				$product['flags'] = 0x42;	// Inventoried, no backorder
+			}
+			$product['units_available'] = $product['inventory_current_num'];
+			if( $product['inventory_current_num'] <= 0 && $product['flags'] == 0x46 ) {
+				$product['flags'] |= 0x0100;
+			}
+		} else {
+			$product['limited_units'] = 'no';
+			$product['units_available'] = 0;
+		}
+
+		return array('stat'=>'ok', 'item'=>$product);
+	}
+
+
 	return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'1963', 'msg'=>'No product specified.'));		
 }
 ?>

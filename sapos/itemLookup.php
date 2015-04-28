22c2bf3a364e0b33663ed2b646e86349f92ca9a8
Andrew Rivett <veggiefrog@gmail.com>
2015-04-28 17:28:53 -0400

    Fixed bug

--- sapos/itemLookup.php
+++ sapos/itemLookup.php
@@ -12,62 +12,11 @@
 //
 function ciniki_products_sapos_itemLookup($ciniki, $business_id, $args) {
 
+	error_log(print_r($args, true));
 	if( !isset($args['object']) || $args['object'] == '' 
 		|| !isset($args['object_id']) || $args['object_id'] == '' ) {
 		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'1960', 'msg'=>'No product specified.'));
 	}
-
-	//
-	// Lookup the specified item based on the product id
-	//
-	// Currently Broken, must use prices sub table
-/*	if( $args['object'] == 'ciniki.products.product' ) {
-		$strsql = "SELECT ciniki_products.id, "
-			. "ciniki_products.parent_id, "
-			. "ciniki_products.code, "
-			. "ciniki_products.name, "
-			. "ciniki_products.price, "
-			. "'' AS price_name, "
-			. "ciniki_products.unit_discount_amount, "
-			. "ciniki_products.unit_discount_percentage, "
-			. "inventory_flags, inventory_current_num, "
-			. "ciniki_products.taxtype_id, "
-			. "ciniki_product_types.object_def "
-			. "FROM ciniki_products "
-			. "LEFT JOIN ciniki_product_types ON ("
-				. "ciniki_products.type_id = ciniki_product_types.id "
-				. "AND ciniki_products.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
-				. ") "
-			. "WHERE ciniki_products.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
-			. "AND ciniki_products.id = '" . ciniki_core_dbQuote($ciniki, $args['object_id']) . "' "
-			. "";
-		ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryIDTree');
-		$rc = ciniki_core_dbHashQueryIDTree($ciniki, $strsql, 'ciniki.products', array(
-			array('container'=>'products', 'fname'=>'id',
-				'fields'=>array('id', 'parent_id', 'code', ''description'=>'name',
-					'unit_amount'=>'price', 'unit_discount_amount', 'unit_discount_percentage',
-					'inventory_flags', 'inventory_current_num', 
-					'taxtype_id')),
-			));
-		if( $rc['stat'] != 'ok' ) {
-			return $rc;
-		}
-		if( !isset($rc['products']) || count($rc['products']) < 1 ) {
-			return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'1961', 'msg'=>'No product found.'));		
-		}
-		$product = array_pop($rc['products']);
-		// Check if product has inventory or unlimited
-		if( ($product['inventory_flags']&0x01) > 0 ) {
-			$product['limited_units'] = 'yes';
-			$product['units_available'] = $product['inventory_current_num'];
-		} else {
-			$product['limited_units'] = 'no';
-			$product['units_available'] = 0;
-		}
-
-		return array('stat'=>'ok', 'item'=>$product);
-	} 
-*/
 
 	//
 	// Lookup the requested product if specified along with a price_id
@@ -144,6 +93,7 @@ function ciniki_products_sapos_itemLookup($ciniki, $business_id, $args) {
 		&& isset($args['object_id']) && $args['object_id'] > 0
 		&& isset($args['pricepoint_id']) && $args['pricepoint_id'] > 0 
 		) {
+		error_log('test');
 		$strsql = "SELECT ciniki_products.id, "
 			. "ciniki_products.parent_id, "
 			. "ciniki_products.code, "
@@ -203,6 +153,74 @@ function ciniki_products_sapos_itemLookup($ciniki, $business_id, $args) {
 		return array('stat'=>'ok', 'item'=>$product);
 	}
 
+	//
+	// Lookup the specified item based on the product id
+	//
+	// Currently Broken, must use prices sub table
+	if( $args['object'] == 'ciniki.products.product' ) {
+		$strsql = "SELECT ciniki_products.id, "
+			. "ciniki_products.parent_id, "
+			. "ciniki_products.code, "
+			. "ciniki_products.name, "
+			. "ciniki_products.price, "
+			. "'' AS price_name, "
+			. "ciniki_products.unit_discount_amount, "
+			. "ciniki_products.unit_discount_percentage, "
+			. "inventory_flags, inventory_current_num, "
+			. "ciniki_products.taxtype_id, "
+			. "ciniki_product_types.object_def "
+			. "FROM ciniki_products "
+			. "LEFT JOIN ciniki_product_types ON ("
+				. "ciniki_products.type_id = ciniki_product_types.id "
+				. "AND ciniki_products.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+				. ") "
+			. "WHERE ciniki_products.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+			. "AND ciniki_products.id = '" . ciniki_core_dbQuote($ciniki, $args['object_id']) . "' "
+			. "";
+		ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryIDTree');
+		$rc = ciniki_core_dbHashQueryIDTree($ciniki, $strsql, 'ciniki.products', array(
+			array('container'=>'products', 'fname'=>'id',
+				'fields'=>array('id', 'parent_id', 'code', 'description'=>'name',
+					'unit_amount'=>'price', 'unit_discount_amount', 'unit_discount_percentage',
+					'inventory_flags', 'inventory_current_num', 
+					'taxtype_id')),
+			));
+		if( $rc['stat'] != 'ok' ) {
+			return $rc;
+		}
+		if( !isset($rc['products']) || count($rc['products']) < 1 ) {
+			return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'1961', 'msg'=>'No product found.'));		
+		}
+		$product = array_pop($rc['products']);
+		// Check if product has inventory or unlimited
+		if( ($product['inventory_flags']&0x01) > 0 ) {
+			$product['limited_units'] = 'yes';
+			$product['units_available'] = $product['inventory_current_num'];
+		} else {
+			$product['limited_units'] = 'no';
+			$product['units_available'] = 0;
+		}
+		// Check if product has inventory or unlimited
+		$product['flags'] = 0;
+		if( ($product['inventory_flags']&0x01) > 0 ) {
+			if( ($product['inventory_flags']&0x02) > 0 ) {
+				$product['limited_units'] = 'no';
+				$product['flags'] = 0x46;	// Inventoried and backorder available
+			} else {
+				$product['limited_units'] = 'yes';
+				$product['flags'] = 0x42;	// Inventoried, no backorder
+			}
+			$product['units_available'] = $product['inventory_current_num'];
+			if( $product['inventory_current_num'] <= 0 && $product['flags'] == 0x46 ) {
+				$product['flags'] |= 0x0100;
+			}
+		} else {
+			$product['limited_units'] = 'no';
+			$product['units_available'] = 0;
+		}
+		error_log(print_r($product, true));
+		return array('stat'=>'ok', 'item'=>$product);
+	} 
 
 	return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'1963', 'msg'=>'No product specified.'));		
 }

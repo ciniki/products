6be1477b812f8879cc422c7fdbdf9a181dd184ca
Andrew Rivett <veggiefrog@gmail.com>
2016-05-07 10:51:05 -0400

    Fixed bug with removed web indexed objects

--- hooks/webIndexObject.php
+++ hooks/webIndexObject.php
@@ -43,7 +43,7 @@ function ciniki_products_hooks_webIndexObject($ciniki, $business_id, $args) {
             return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'3282', 'msg'=>'Object not found'));
         }
         if( !isset($rc['item']) ) {
-            return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'3283', 'msg'=>'Object not found'));
+            return array('stat'=>'noexist', 'err'=>array('pkg'=>'ciniki', 'code'=>'3283', 'msg'=>'Object not found'));
         }
 
         //
